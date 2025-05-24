efeb24c6b6762ec57b5995566b9db047814bf9e3
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-05-24 18:28:16 -0500

    En la clase Main se ejecutara el codigo, Se importa libreria "Scanner" para captura de Datos desde teclado

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; //Se importa Libreria SCANNER para la captura de Datos des Consola
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Persona[] personas = new Persona[5]; // Almacena 5 Personas
+
