7a8e4456811bb95824fff4d5c79aeb9884add6fc
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-05-24 19:47:48 -0500

    PLUS: En clase Main se agregan estructuras de control en captura de GENERO y EDAD, utilizando ciclos While e If-Else validando que datos sean correctos y esperados, doc: Actualizaciones en clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.Scanner; //Se importa Libreria SCANNER para la captura de Datos des Consola
-
+import java.util.Scanner; //Se importa Libreria SCANNER para la captura de Datos desde Consola
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,16 +8,41 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println("Ingrese el NOMBRE de la persona " + (i + 1) + ":");
             String nombre = scanner.nextLine();
-            System.out.println("Ingrese el APELLIDO:");
+            System.out.println("Ingrese el APELLIDO de la persona " + (i + 1) + ":");
             String apellido = scanner.nextLine();
-            System.out.println("Ingrese el GENERO (Masculino/Femenino):");
-            String genero = scanner.nextLine();
-            System.out.println("Ingrese la EDAD (Años Cumplidos):");
-            int edad = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
+
+            // PLUS - Validación de género (Con las estructura del ciclo While se controlan los valores correctos y esperados)
+            String genero;
+            while (true) {
+                System.out.println("Ingrese el GENERO (Masculino/Femenino) de la persona " + (i + 1) + ":");
+                genero = scanner.nextLine();
+                if (genero.equalsIgnoreCase("Masculino") || genero.equalsIgnoreCase("Femenino")) {
+                    break;
+                } else {
+                    System.out.println("Género no válido. Ingrese 'Masculino' o 'Femenino'.");
+                }
+            }
+            // PLUS - Validación de edad en el rango de 1 a 120 (se controla que sea un numero entero valido y logico)
+            int edad;
+            while (true) {
+                System.out.println("Ingrese EDAD de la persona " + (i + 1) + " en Años Cumplidos (entre 1 y 120):");
+                if (scanner.hasNextInt()) {
+                    edad = scanner.nextInt();
+                    scanner.nextLine(); // Limpiar el buffer de entrada
+                    if (edad >= 1 && edad <= 120) {
+                        break;
+                    } else {
+                        System.out.println("Edad no válida. Ingrese un número entre 1 y 120.");
+                    }
+                } else {
+                    System.out.println("Entrada no válida. Ingrese un número entero.");
+                    scanner.nextLine(); // Limpiar el buffer de entrada
+                }
+            }
 
             personas[i] = new Persona(nombre, apellido, genero, edad);
         }
+
         //MOSTRAR nombres y generos de las 5 personas
         mostrarNombresYGeneros(personas);
 
@@ -34,6 +58,7 @@ public class Main {
 
     //B) METODO para RETORNAR nombres y géneros
     public static void mostrarNombresYGeneros(Persona[] personas) {
+        System.out.println(); // Línea vacía para Mejorar visualizacion de datos en consola
         System.out.println("Lista de nombres y generos:");
         for (Persona persona : personas) {
             System.out.println("Nombre: " + persona.getNombre() + ", Género: " + persona.getGenero());
@@ -42,6 +67,7 @@ public class Main {
 
     //C) METODO para CALCULAR PROMEDIO de edad
     public static double calcularPromedioEdad(Persona[] personas) {
+        System.out.println(); // Línea vacía para Mejorar visualizacion de datos en consola
         int sumaEdades = 0;
         for (Persona persona : personas) {
             sumaEdades += persona.getEdad();
